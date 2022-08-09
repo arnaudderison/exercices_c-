cd081a73e084f4294214f3d8effc1b3ce9b7bb6d
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-08-09 18:00:43 +0100

    Exercice String 46

---
+++ exercices/exercices/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercices
+{
+    internal class Programme
+    {
+        static void Main(string[] args)
+        {
+            string TheAlert = "";
+            TheAlert = FindChar("ezeqsdb");   
+            Console.WriteLine(TheAlert);
+        }
+
+        // https://www.w3resource.com/csharp-exercises/basic-algo/index.php • exercies 46 - verrification de chaine de caractère
+        public static string FindChar(string chaine)
+        {
+            string result = "";
+
+            if(chaine.StartsWith('f'))
+            {
+                result += "Fizz";
+            }
+            if (chaine.EndsWith("b"))
+            {
+                result += "Buzz";
+            }
+            if (String.IsNullOrEmpty(result))
+            {
+                result += "Aucun des deux";
+            }
+            return result;
+        }
+    }
+}
